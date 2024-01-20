6b8bf2cc3a56bb29f56f093b61a32bbbe4d8b20a
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-01-20 15:33:10 +0545

    add: 10 single number - XOR

--- 10-single-number.py
+++ 10-single-number.py
@@ -1,12 +1,19 @@
-def single_number(arr):
-    temp = set()
-    for i in arr:
-        if i in temp:
-            temp.remove(i)
-        else:
-            temp.add(i)
-    print(temp.pop())
+# def single_number(nums):
+#     temp = set()
+#     for i in nums:
+#         if i in temp:
+#             temp.remove(i)
+#         else:
+#             temp.add(i)
+#     print(temp.pop())
 
+def single_number(nums):
+    sum = 0
+    for num in nums:
+        sum = sum ^ num
+    print(sum)
+
+# Examples:
 
 single_number([2, 2, 1])  # 1
 single_number([4, 1, 2, 1, 2])  # 4
