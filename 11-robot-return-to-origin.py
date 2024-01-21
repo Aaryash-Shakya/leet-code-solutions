c33361ab6f7b429a90bd2cc36d4683d27d6632c3
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-01-21 18:33:04 +0545

    add: 11 robot return to origin

---
+++ 11-robot-return-to-origin.py
@@ -0,0 +1,22 @@
+def robot_returns(moves):
+    x_disp = 0
+    y_disp = 0
+    for move in moves:
+        if move == "U":
+            y_disp += 1
+        elif move == "D":
+            y_disp -= 1
+        elif move == "L":
+            x_disp -= 1
+        elif move == "R":
+            x_disp += 1
+    if x_disp == 0 and y_disp == 0:
+        print(True)
+    else:
+        print(False)
+
+# Examples:
+robot_returns("UD")  # True
+robot_returns("LL")  # False
+robot_returns("RRDD")  # False
+robot_returns("UURRLLDD")  # True
