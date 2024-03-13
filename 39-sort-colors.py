6bd829343a55c88748f94a6471cb1faaa3ef235f
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-03-13 18:33:56 +0545

    add: 39 sort colors - dutch national flag approach

--- 39-sort-colors.py
+++ 39-sort-colors.py
@@ -1,4 +1,5 @@
-# counting approach O(n)
+# counting approach O(2n)
+'''
 def sort_colors(nums):
     # initialize counters
     count_0, count_1, count_2 = 0, 0, 0
@@ -20,6 +21,25 @@ def sort_colors(nums):
             nums[i] = 1
         else:
             nums[i] = 2
+'''
+
+# dutch national flag algorithm O(n)
+# slightly better as it only traverses the list once
+def sortColors(nums):
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+
+    while mid <=high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] ==1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
 
 
 # Examples:
