7c4ab20f04ea8326536926e23e352c69084f3000
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2024-03-13 18:28:40 +0545

    chore: add comments and examples to binary tree traversal algos

--- 36-binary-tree-preorder-traversal.py
+++ 36-binary-tree-preorder-traversal.py
@@ -8,13 +8,23 @@
 
 def preorder_traversal(root):
     path = []
+
     def explore_node(node):
         if node is None:
             return
         else:
+            # add current node's value
             path.append(node.val)
+            # explore left subtree
             explore_node(node.left)
+            # explore right subtree
             explore_node(node.right)
 
-    explore_node(root)    
+    explore_node(root)
     return path
+
+
+# Examples:
+# root = TreeNode(1, None, TreeNode(2, TreeNode(3), None)) = [1, None, 2, 3]  # [1, 2, 3]
+# root = TreeNode(1, TreeNode(2), TreeNode(3)) = [1, 2, 3]  # [1, 2, 3]
+# root = TreeNode(1) = [1]  # [1]
--- 38-binary-tree-inorder-traversal.py
+++ 38-binary-tree-inorder-traversal.py
@@ -6,15 +6,27 @@
 #         self.right = right
 
 
-def postorder_traversal(root):
+def inorder_traversal(root):
     path = []
+
     def explore_node(node):
+        # base case
         if node is None:
             return
+
         else:
+            # explore left subtree
             explore_node(node.left)
+            # add current node's value
             path.append(node.val)
+            # explore right subtree
             explore_node(node.right)
 
-    explore_node(root)    
+    explore_node(root)
     return path
+
+
+# Examples:
+# root = TreeNode(1, None, TreeNode(2, TreeNode(3), None)) = [1, None, 2, 3]  # [1, 3, 2]
+# root = TreeNode(1, TreeNode(2), TreeNode(3)) = [1, 2, 3]  # [2, 1, 3]
+# root = TreeNode(1) = [1]  # [1]
